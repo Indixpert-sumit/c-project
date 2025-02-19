b69052aa9a4c4acffdd23f002080ea9bd69dde52
Indixpert-sumit <sy0050854@gmail.com>
2025-02-19 09:38:55 +0530

    menu

--- mytask/name-1/practice/bankmanagment/bank.c
+++ mytask/name-1/practice/bankmanagment/bank.c
@@ -4,15 +4,15 @@ int main()
 
     int num;
     
-    printf("\t! welcome to world's bank !");
-    printf("\n! Press 1 For Create Account !");
-    printf("\n! press 2 For check balance !");
-    printf("\n! Press 3 For deposit !");
+             printf("\t! welcome to world's bank !");
+                 printf("\n! Press 1 For Create Account !");
+                     printf("\n! press 2 For check balance !");
+              printf("\n! Press 3 For deposit !");
 
 
     
-    printf("\n! please press  any option !");
-    scanf("%d",&num);
+         printf("\n! please press  any option !");
+     scanf("%d",&num);
 
 
 return 0;
